25db462e34deff1e233c4ae968b1125893bccd64
Keerthana Gujja <keerthana@Keerthanas-MacBook-Air.local>
2026-07-12 13:19:57 +0530

    Remove private pages from sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,19 +8,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "daily",
       priority: 1,
     },
-
-    {
-      url: "https://new-maruthi-tiffins.vercel.app/profile",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.8,
-    },
-
-    {
-      url: "https://new-maruthi-tiffins.vercel.app/orders",
-      lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.8,
-    }
   ];
 }
